ec8a5a3bb3d24f3d57769c53d22f3d5c09d6759a
David Lau <david.kaimun.lau@gmail.com>
2022-02-14 18:35:50 +0100

    react app created and nav placeholder added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import './App.css';
+import Navigation from './components/navigation/Navigation';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        {/* <Logo />
+        <ImageLinkForm />
+        <FaceRecognition /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
