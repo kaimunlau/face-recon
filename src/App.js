fc15d0d2394f3feeb31c1fbb3b72a503d995617f
David Lau <david.kaimun.lau@gmail.com>
2022-02-14 19:12:08 +0100

    added tilting logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Component } from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        {/* <Logo />
-        <ImageLinkForm />
+        <Logo />
+        {/* <ImageLinkForm />
         <FaceRecognition /> */}
       </div>
     );
